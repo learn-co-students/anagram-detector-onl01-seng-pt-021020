ed82800a147cc65cab67f948ed3d180f0dd6556f
Andrew Hinojosa <drews_view@hotmail.com>
2020-03-05 20:29:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
+require "pry"
+
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :name 
+  
+  def initialize(word)
+    @name = word
+  end
+  
+  def match(words)
+    words.select {|w| w.split("").sort == name.split("").sort }
+  end
+  
+end
