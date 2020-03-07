cd404fc555848e864e8ecdcea87b9c2418bb0cee
treylavergne <treylavergne@gmail.com>
2020-03-07 11:16:00 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word_finder
+
+    def initialize(word_finder)
+        @word_finder = word_finder
+    end
+
+    def match(word_array)
+        word_array.select {|word| word.split("").sort == @word_finder.split("").sort }
+
+
+
+
+    end
+
+
+
+
+
+end
