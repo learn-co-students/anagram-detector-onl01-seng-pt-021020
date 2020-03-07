7826d54aa23870986ad48015fb8d1d737df0c723
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-07 17:23:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
+require "pry"
 # Your code goes here!
+class Anagram
+  attr_accessor :phrase
+
+  def initialize(phrases)
+    @phrase = phrases
+  end
+
+  def match(array)
+    array.select {|ph|
+      ph.split("").sort == @phrase.split("").sort}
+  end
+end
